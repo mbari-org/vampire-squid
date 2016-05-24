0d97a01d663064cb21db9e8f95a29d888cef424f
Brian Schlining <bschlining@gmail.com>
2016-05-23 18:40:16 -0700

    check point

--- src/main/scala/org/mbari/vars/vam/api/FailedRequest.scala
+++ src/main/scala/org/mbari/vars/vam/api/FailedRequest.scala
@@ -6,6 +6,4 @@ package org.mbari.vars.vam.api
   * @author Brian Schlining
   * @since 2016-05-23T18:38:00
   */
-class FailedRequest {
-
-}
+case class FailedRequest(message: String, exception: Throwable = null)
--- src/main/scala/org/mbari/vars/vam/api/VideoSequenceV1Api.scala
+++ src/main/scala/org/mbari/vars/vam/api/VideoSequenceV1Api.scala
@@ -4,13 +4,11 @@ import java.util.UUID
 
 import org.mbari.vars.vam.controllers.VideoSequenceController
 import org.mbari.vars.vam.dao.jpa.VideoSequence
-import org.scalatra.swagger.{ Swagger, SwaggerSupport }
+import org.scalatra.swagger.{ Swagger }
 import org.slf4j.LoggerFactory
 import org.scalatra._
 
-import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.control.NonFatal
-import org.scalatra.util.conversion.TypeConverter
+import scala.concurrent.{ ExecutionContext }
 import scala.collection.JavaConverters._
 
 /**
@@ -23,6 +21,7 @@ class VideoSequenceV1Api(controller: VideoSequenceController)(implicit val swagg
     extends APIStack {
 
   private[this] val log = LoggerFactory.getLogger(getClass)
+  private[this] val textHeader = Map("Content-Type" -> "text/plain")
 
   override protected def applicationDescription: String = "Video Sequence API (v1)"
 
@@ -48,7 +47,9 @@ class VideoSequenceV1Api(controller: VideoSequenceController)(implicit val swagg
   get("/:uuid", operation(uuidGET)) {
     val uuid = params.getAs[UUID]("uuid").getOrElse(halt(BadRequest("Please provide a valid UUID")))
     controller.findByUUID(uuid).map({
-      case None => halt(NotFound(s"A video-sequence with a UUID of $uuid was not found in the database"))
+      case None => halt(NotFound(
+        headers = textHeader,
+        reason = s"A video-sequence with a UUID of $uuid was not found in the database"))
       case Some(vs) => controller.toJson(vs)
     })
   }
@@ -59,45 +60,37 @@ class VideoSequenceV1Api(controller: VideoSequenceController)(implicit val swagg
       pathParam[String]("name").description("The name of the video sequence")))
 
   get("/name/:name", operation(nameGET)) {
-    params.get("name") match {
-      case Some(name) => controller.findByName(name).map({
-        case Some(vs) => controller.toJson(vs)
-        case None => NotFound
-      })
-      case None => BadRequest
-    }
+    val name = params("name")
+    controller.findByName(name).map({
+      case None => {
+        halt(NotFound(s"A video-sequence with a name of '$name' was not found in the database", headers = textHeader))
+      }
+      case Some(vs) => controller.toJson(vs)
+    })
   }
 
   val vsPOST = (apiOperation[Unit]("createPOST")
     summary "Create a video sequence"
     parameters ())
 
+  // TODO post should require authentication
   post("/") {
-    val name = params("name")
-    val cameraID = params("camera_id")
-    if (name == null || cameraID == null) {
-      UnprocessableEntity(reason = "Both name and cameraID parameters are required. One or both were missing")
-    } else {
-      try {
-        controller.create(name, cameraID)
-          .map(vs => Created(controller.toJson(vs), headers = Map("Location" -> s"/${vs.uuid}")))
-      } catch {
-        case NonFatal(e) => BadRequest(reason = "Something bad happened. " + e.getMessage)
-      }
-    }
-
+    val name = params.get("name").getOrElse(halt(BadRequest("A 'name' parameter is required", headers = textHeader)))
+    val cameraID = params.get("camera_id").getOrElse(halt(BadRequest("A 'camera_id' parameter is required")))
+    controller.create(name, cameraID)
+      .map(controller.toJson)
   }
 
+  // TODO delete should require authentication
   delete("/:uuid") {
-    params.getAs[UUID]("uuid") match {
-      case Some(uuid) => controller.delete(uuid).map({
-        case true => NoContent
-        case false => NotFound
-      })
-      case None => BadRequest(reason = "No UUID was found.")
-    }
+    val uuid = params.getAs[UUID]("uuid").getOrElse(halt(BadRequest("A UUID parameter is required", headers = textHeader)))
+    controller.delete(uuid).map({
+      case true => halt(NoContent(reason = s"Success! Deleted video-sequence with UUID of $uuid", headers = textHeader))
+      case false => halt(NotFound(s"Failed. No video-sequence with UUID of $uuid was found.", headers = textHeader))
+    })
   }
 
+  // TODO put should update values
   put("/:uuid") {
 
   }
