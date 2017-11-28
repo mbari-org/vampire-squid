0d1919c489d11613b33cff1463f83d644f5ee3df
Brian Schlining <bschlining@gmail.com>
2017-11-27 21:53:25 -0800

    Fixed date parsing

--- src/main/scala/org/mbari/vars/vam/api/APIStack.scala
+++ src/main/scala/org/mbari/vars/vam/api/APIStack.scala
@@ -1,12 +1,13 @@
 package org.mbari.vars.vam.api
 
 import java.net.URI
-import java.time.{ Duration, Instant }
+import java.time.format.DateTimeFormatter
+import java.time.{Duration, Instant}
 import java.util.UUID
 import javax.servlet.http.HttpServletRequest
 
 import org.mbari.vars.vam.dao.jpa.ByteArrayConverter
-import org.scalatra.{ ContentEncodingSupport, FutureSupport, NotFound, ScalatraServlet }
+import org.scalatra.{ContentEncodingSupport, FutureSupport, NotFound, ScalatraServlet}
 import org.scalatra.swagger.SwaggerSupport
 import org.scalatra.util.conversion.TypeConverter
 
@@ -32,12 +33,17 @@ abstract class APIStack extends ScalatraServlet
     response.headers += ("Access-Control-Allow-Origin" -> "*")
   }
 
+  protected[this] val timeFormatter = DateTimeFormatter.ISO_DATE_TIME
+
   implicit val stringToUUID = new TypeConverter[String, UUID] {
     override def apply(s: String): Option[UUID] = Try(UUID.fromString(s)).toOption
   }
 
   implicit val stringToInstant = new TypeConverter[String, Instant] {
-    override def apply(s: String): Option[Instant] = Try(Instant.parse(s)).toOption
+    override def apply(s: String): Option[Instant] = {
+      //Try(Instant.parse(s)).toOption
+      Try(Instant.from(timeFormatter.parse(s))).toOption
+    }
   }
 
   implicit val stringToDuration = new TypeConverter[String, Duration] {
