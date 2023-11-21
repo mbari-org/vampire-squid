5e544ed0f97bef3ea726050047f4160dba160a98
Brian Schlining <bschlining@gmail.com>
2023-11-20 17:27:05 -0800

    #6 - slight cleanup

--- it-sqlserver/src/test/scala/org/mbari/vampiresquid/repository/jpa/SqlServerDAOFactory.scala
+++ it-sqlserver/src/test/scala/org/mbari/vampiresquid/repository/jpa/SqlServerDAOFactory.scala
@@ -1,22 +1,20 @@
 package org.mbari.vampiresquid.repository.jpa
 
-import org.testcontainers.containers.MSSQLServerContainer
-import org.testcontainers.utility.DockerImageName
 import jakarta.persistence.EntityManagerFactory
-import org.testcontainers.containers.JdbcDatabaseContainer
 import org.mbari.vampiresquid.etc.tc.AzureSqlEdgeContainerProvider
 
 object SqlServerDAOFactory extends SpecDAOFactory:
 
   val container = new AzureSqlEdgeContainerProvider().newInstance()
 
-  // THe image name must match the one in src/test/resources/container-license-acceptance.txt
+  // The image name must match the one in src/test/resources/container-license-acceptance.txt
   // val container = new MSSQLServerContainer(DockerImageName.parse("mcr.microsoft.com/mssql/server:2019-latest"))
   // container.acceptLicense()
   container.withInitScript("sql/mssqlserver/02_m3_video_assets.sql")
   container.withReuse(true)
+  container.start()
 
-  override def beforeAll(): Unit = container.start()
+//  override def beforeAll(): Unit = container.start()
   // NOTE: calling container.stop() after each test causes the tests to lose the connection to the database.
   // I'm using a shutdown hook to close the container at the end of the tests.
   //  override def afterAll(): Unit  = container.stop()
